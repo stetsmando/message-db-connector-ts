fa76d1cf9c2e70a67ea1bf24adb829a6972a30f0
Stetson Pierce <stetsmando@gmail.com>
2023-03-16 20:14:08 -0600

    fix(in-memory-store): fix bad uuid import

--- src/in-memory-store/in-memory-writer.test.ts
+++ src/in-memory-store/in-memory-writer.test.ts
@@ -1,4 +1,4 @@
-import { v4 as uuid } from 'uuid';
+import { randomUUID as uuid } from 'crypto';
 import { InMemoryWriter } from './in-memory-writer';
 import { InMemoryStore } from './store';
 import { Message, MessageOptions } from '..';
